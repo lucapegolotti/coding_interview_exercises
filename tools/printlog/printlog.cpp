5acb2f60eb1be41fbbe4b8249693c427a5665764
Luca Pegolotti <luca.pegolotti@epfl.ch>
2017-08-16 10:07:59 +0200

    Bugfix in printlog

--- tools/printlog/printlog.cpp
+++ tools/printlog/printlog.cpp
@@ -7,16 +7,15 @@ void printlog(Color outColor, std::string text)
   std::cout << "\033[0m";
 }
 
-
-void printlog(Color outColor, int num)
-{
-  printlog(outColor, std::to_string(num));
-}
-
 template < typename T >
 std::string to_string( const T& n )
 {
     std::ostringstream stm ;
     stm << n ;
     return stm.str() ;
+}
+
+void printlog(Color outColor, int num)
+{
+  printlog(outColor, to_string(num));
 }
--- tools/printlog/printlog.hpp
+++ tools/printlog/printlog.hpp
@@ -8,9 +8,10 @@
 enum Color {BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
 
 extern void printlog(Color outColor, std::string text);
-extern void printlog(Color outColor, int num);
 
 template < typename T >
 extern std::string to_string( const T& n );
+
+extern void printlog(Color outColor, int num);
 
 #endif  // PRINTLOG_HPP
